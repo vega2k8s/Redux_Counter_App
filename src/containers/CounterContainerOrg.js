f1ae06891953a209199bdc739a68bc5ef70b746e
vega2k8s <vega2k8s@gmail.com>
2025-05-22 17:31:48 +0900

    connect() 함수를 심플하게

--- src/containers/CounterContainerOrg.js
+++ src/containers/CounterContainerOrg.js
@@ -1,9 +1,13 @@
 import Counter from '../components/Counter';
-//import * as actions from '../actions';
-import { increment, decrement, setColor} from '../actions';
+import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { getRandomColor } from '../utils';
 
+// state 객체에 포함된 상태 변수를  Counter 컴포넌트에게 props로 전달합니다.
+const mapStateToProps = (state) => ({
+    color: state.color,
+    number: state.number
+});
 
 /* 액션 생성 함수를 사용하여 액션을 생성하고,
    해당 액션을 dispatch하는 함수를 만든 후, 
@@ -19,13 +23,8 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 /*Counter 컴포넌트를 애플리케이션의 데이터 레이어와 묶는 역할을 합니다.*/
-const CounterContainer = connect(
-    (state) => ({
-        color: state.color,
-        number: state.number
-    }),
-    {
-
-    }
+const CounterContainerOrg = connect(
+    mapStateToProps,
+    mapDispatchToProps
 )(Counter);
-export default CounterContainer;
+export default CounterContainerOrg;
