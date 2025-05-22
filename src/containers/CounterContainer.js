bb5b2b513ad1bb2b68de57a617e71c0216d6a976
vega2k8s <vega2k8s@gmail.com>
2025-05-22 17:36:36 +0900

    connect() 함수를 심플하게

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -9,14 +9,14 @@ import { getRandomColor } from '../utils';
    해당 액션을 dispatch하는 함수를 만든 후, 
    함수의 property 이름을 Counter 컴포넌트에게 props로 전달합니다.
 */
-const mapDispatchToProps = (dispatch) => ({
-    increment: () => dispatch(actions.increment()),
-    onDecrement: () => dispatch(actions.decrement()),
-    onSetColor: () => {
-        //const color = 'red' // 임시 getRandomColor()를 작성하여 변경할 예정
-        dispatch(actions.setColor(getRandomColor()));
-    }
-});
+// const mapDispatchToProps = (dispatch) => ({
+//     increment: () => dispatch(actions.increment()),
+//     onDecrement: () => dispatch(actions.decrement()),
+//     onSetColor: () => {
+//         //const color = 'red' // 임시 getRandomColor()를 작성하여 변경할 예정
+//         dispatch(actions.setColor(getRandomColor()));
+//     }
+// });
 
 /*Counter 컴포넌트를 애플리케이션의 데이터 레이어와 묶는 역할을 합니다.*/
 const CounterContainer = connect(
@@ -25,7 +25,9 @@ const CounterContainer = connect(
         number: state.number
     }),
     {
-
+        increment,
+        onDecrement: decrement,
+        onSetColor: () => setColor(getRandomColor())
     }
 )(Counter);
 export default CounterContainer;
